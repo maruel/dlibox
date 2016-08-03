6cf3c1c51eda44d7b48ccc18ac14a84c6090cf1d
Marc-Antoine Ruel <maruel@chromium.org>
2016-08-02 22:38:35 -0400

    Fine tune the setup scripts.

--- go/setup/dlibox_firstboot.sh
+++ go/setup/dlibox_firstboot.sh
@@ -24,37 +24,63 @@ exec 1<>$LOG_FILE
 # Redirect stderr to stdout.
 exec 2>&1
 
-echo "- Configuring SSH, SPI, I2C"
-sed -i 's/PasswordAuthentication yes/#PasswordAuthentication no/' /etc/ssh/sshd_config
+
+echo "- Configuring SSH, SPI, I2C, GPU memory"
+# Force key based authentication since the password is known.
+sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
 # https://github.com/RPi-Distro/raspi-config/blob/master/raspi-config
+# 0 means enabled.
 raspi-config nonint do_ssh 0
 raspi-config nonint do_spi 0
 raspi-config nonint do_i2c 0
+# Lowers GPU memory from 64Mb to 16Mb. Doing so means goodbye to startx.
+raspi-config nonint do_memory_split 16
+
+
+echo "- Configuring locale as French Canadian"
+# Use the best keyboard layout. Change to "us" if needed. :)
+sed -i s/XKBLAYOUT="gb"/XKBLAYOUT="ca"/ /etc/default/keyboard
+# Use "timedatectl list-timezones" to list the values.
+timedatectl set-timezone America/Toronto
+# Switch to en_US.
+locale-gen --purge en_US.UTF-8
+sed -i s/en_GB/en_US/ /etc/default/locale
+# Fix Wifi country settings for Canda.
+raspi-config nonint do_wifi_country CA
+
 
 echo "- Updating OS"
 apt-get update
 apt-get upgrade -y
 apt-get install -y git ifstat ntpdate sysstat tmux vim
 
+
+# TODO(maruel): Do not forget to update the Go version as needed.
 echo "- Installing as user"
-sudo -u pi -- <<EOF
-mkdir /home/pi/bin
-cd /home/pi; git clone --recurse https://github.com/maruel/bin_pub /home/pi/bin/bin_pub
-/home/pi/bin/bin_pub/setup/update_config.py
-/home/pi/bin/bin_pub/setup/install_golang.py
+sudo -u pi -- <<'EOF'
+cd
+mkdir src
+git clone --recurse https://github.com/maruel/bin_pub bin/bin_pub
+bin/bin_pub/setup_scripts/update_config.py
+export GOROOT=/home/pi/src/golang
+curl -S https://storage.googleapis.com/golang/go1.6.3.linux-armv6l.tar.gz | tar xz
+mv go src/golang
+#bin/bin_pub/setup/install_golang.py
+PATH="$PATH:$GOROOT/bin"
+export GOPATH="$HOME/src/gopath"
 go get github.com/maruel/dlibox/go/cmd/dlibox
 EOF
 
-# TODO(maruel): Change locale & keyboard layout (?)
-# dpkg-reconfigure locales
-# TODO(maruel): Change timezone.
-# dpkg-reconfigure tzdata
-# raspi-config nonint do_wifi_country CA
-# TODO(maruel): Change hostname to unique name.
-# raspi-config nonint do_hostname dlibox-<serial>
-# TODO(maruel): Reduce GPU memory.
+# TODO(maruel): Setup dlibox.service
+
+
+# TODO(maruel): Change hostname to unique name. The problem is that it becomes
+# harder to find the device on the network.
+#SERIAL=$(cat /proc/cpuinfo | grep Serial | cut -d ":" -f 2 | sed 's/^ 0\+//')
+# raspi-config nonint do_hostname dlibox-$SERIAL
+
 # TODO(maruel): avahi browse to detect if MQTT is installed, install otherwise.
-# TODO(maruel): Change 'pi' password?
+# TODO(maruel): kbdrate -d 200 -r 60
 
 echo "- Done, rebooting"
 reboot
