2de27da014aeb7e8dcc002611f58c1c498f0fde7
Marc-Antoine Ruel <maruel@chromium.org>
2016-08-09 13:39:01 -0400

    Add note about openhab, improve README.md

--- go/setup/README.md
+++ go/setup/README.md
@@ -58,6 +58,32 @@ Anytime you `go install github.com/maruel/dlibox/go/cmd/dlibox`, systemd will
 restart dlibox automatically.
 
 
-## Logs
+## Debugging
+
+
+### mDNS
+
+[Bonjour
+Browser](https://play.google.com/store/apps/details?id=com.grokkt.android.bonjour)
+is a nice Android app to debug mDNS broadcasts.
+
+
+### MQTT
+
+[MQTTLens](https://chrome.google.com/webstore/detail/mqttlens/hemojaaeigabkbcookmlgmdigohjobjm)
+is a Google Chrome app to debug messages on a MQTT server.
+
+
+### Logs
+
+Look at the logs on the dlibox server:
 
     sudo journalctl -u dlibox
+
+
+### InfraRed receiver
+
+`irw` will print decoded messages by [lirc](http://www.lirc.org/) via the
+[Raspbian specific lirc-rpi kernel
+module](https://github.com/raspberrypi/firmware/blob/master/boot/overlays/README).
+See [rpi/ir.go](../../rpi/ir.go) for more details.
--- go/setup/support/install_dependencies.sh
+++ go/setup/support/install_dependencies.sh
@@ -16,7 +16,8 @@ raspi-config nonint do_i2c 0
 # Lowers GPU memory from 64Mb to 16Mb. Doing so means goodbye to startx.
 raspi-config nonint do_memory_split 16
 
-# lirc
+
+## lirc
 
 apt-get -y install lirc
 
@@ -33,3 +34,28 @@ echo "" >> /boot/config.txt
 echo "# dlibox" >> /boot/config.txt
 echo "# https://github.com/raspberrypi/firmware/blob/master/boot/overlays/README" >> /boot/config.txt
 echo "dtoverlay=lirc-rpi,gpio_in_pull=high" >> /boot/config.txt
+
+
+## MQTT server
+
+apt-get -y install mosquitto
+
+# TODO(maruel): Create file in /etc/mosquitto/conf.d/ as per
+# https://mosquitto.org/man/mosquitto-conf-5.html
+# TODO(maruel): Create self-signed certificate and distribute to the devices.
+# For ESP8266, use library that supports certificates, e.g.
+# https://github.com/tuanpmt/esp_mqtt
+
+
+### OpenHAB
+
+# http://www.openhab.org/getting-started/downloads.html
+# TODO(maruel): Update.
+OPENHAB_DISTRO=distribution-1.8.3
+curl -O https://bintray.com/artifact/download/openhab/bin/$OPENHAB_DISTRO-runtime.zip
+unzip OPENHAB_DISTRO-runtime.zip
+rm OPENHAB_DISTRO-runtime.zip
+https://bintray.com/artifact/download/openhab/bin/$OPENHAB_DISTRO-addons.zip
+unzip OPENHAB_DISTRO-addons.zip
+rm OPENHAB_DISTRO-addons.zip
+# TODO(maruel): Enable and configure.
