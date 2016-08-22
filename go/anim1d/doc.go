2d7165055d424596dacc70b604868f631d3acc89
Marc-Antoine Ruel <maruel@chromium.org>
2016-08-22 14:25:53 -0400

    Update package doc

--- go/anim1d/doc.go
+++ go/anim1d/doc.go
@@ -2,7 +2,10 @@
 // Use of this source code is governed under the Apache License, Version 2.0
 // that can be found in the LICENSE file.
 
-// anim1d is a package to draw 1D (line) animations.
+// Package anim1d draws 1D (line) animations that are stateless.
+//
+// That is, they can be played forward, backward, on multiple devices
+// synchronized with only clock being synchronized.
 //
 // It contains all the building blocks to create animations. All the animations
 // are designed to be stateless and serializable so multiple devices can
--- go/devices/apa102/doc.go
+++ go/devices/apa102/doc.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed under the Apache License, Version 2.0
 // that can be found in the LICENSE file.
 
-// apa102 is a package to drive a strip of APA102 LEDs. It handles the color
-// correction.
+// Package apa102 drives a strip of APA102 LEDs. It handles the color
+// intensity and temperature correction and uses the full 8000:1 dynamic range
+// as supported by the devices.
 package apa102
