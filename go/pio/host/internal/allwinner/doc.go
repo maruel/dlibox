b709538fd684223edb269fb326d92c98b8c1c91a
Marc-Antoine Ruel <maruel@chromium.org>
2016-09-19 09:59:44 -0400

    Add reference to Allwinner H3

--- go/pio/host/internal/allwinner/doc.go
+++ go/pio/host/internal/allwinner/doc.go
@@ -6,7 +6,8 @@
 //
 // Datasheet
 //
-// http://files.pine64.org/doc/datasheet/pine64/Allwinner_A64_User_Manual_V1.0.pdf
+// A64: http://files.pine64.org/doc/datasheet/pine64/Allwinner_A64_User_Manual_V1.0.pdf
+// H3: http://dl.linux-sunxi.org/H3/Allwinner_H3_Datasheet_V1.0.pdf
 //
 // Physical overview: http://files.pine64.org/doc/datasheet/pine64/A64_Datasheet_V1.1.pdf
 package allwinner
