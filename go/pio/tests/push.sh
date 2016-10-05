b2a3792f5d301fab1ef81b708d6cc454f4e9daa0
Marc-Antoine Ruel <maruel@chromium.org>
2016-10-05 10:40:27 -0400

    Updated license headers under pio/.
    Ran:
      sed -i 's/2016 Marc-Antoine Ruel/2016 Google Inc/' $(git ls-files --)

--- go/pio/tests/push.sh
+++ go/pio/tests/push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2016 Marc-Antoine Ruel. All rights reserved.
+# Copyright 2016 Google Inc. All rights reserved.
 # Use of this source code is governed under the Apache License, Version 2.0
 # that can be found in the LICENSE file.
 
